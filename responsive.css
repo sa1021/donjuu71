d5e205064edf4225260d0ac18bb28187454022c8
sa1021 <60912434+sa1021@users.noreply.github.com>
2020-04-19 22:35:16 +0900

    Create responsive.css

---
+++ responsive.css
@@ -0,0 +1,226 @@
+/* タブレット向けレイアウト */
+@media all and (max-width: 1000px) {
+  h2 , .menu h3 , .sita a{
+    font-size:35px;
+  }
+
+  h3 , .menu p , .food p{
+  font-size: 30px;
+  }
+
+  h4 , .ji{
+    font-size:25px;
+  }
+
+  h5{
+    font-size: 20px;
+  }
+
+  .home{
+    height: 250px;
+  }
+
+  .home h3{
+    position: absolute;
+    top:200px;
+    left:30px;
+  }
+
+  .osinagaki{
+    padding-bottom:160px;
+  }
+
+  .navi{
+    height:165px;
+  }
+
+  .tori , .nama , .vege , .niku , .orignal , .sina , .rice , .desert{
+    margin:34px 0;
+  }
+
+  .list{
+    margin:8px 0;
+  }
+
+  .list h4{
+    margin:3px 9px ;
+  }
+
+  .yakitori , .nasu , .sasami , .be , .maru , .yuzu{
+    display: none;
+  }
+
+  .yakitori2 , .nasu2 , .sasami2 , .be2 , .maru2 , .yuzu2{
+    display: block;
+  }
+
+  .yakitori2{
+    position :absolute;
+    top: 680px;
+    right:200px;
+  }
+
+  .sasami2{
+    position :absolute;
+    top: 1150px;
+    right:300px;
+  }
+
+  .nasu2{
+    position :absolute;
+    top: 2220px;
+    right:200px;
+  }
+
+  .be2{
+    position :absolute;
+    top: 2940px;
+    right:200px;
+  }
+
+  .yuzu2{
+    position :absolute;
+    top: 3630px;
+    right:270px;
+  }
+
+  .maru2{
+    position :absolute;
+    top: 4750px;
+    right:50px;
+  }
+
+
+}
+/* スマホ向けレイアウト */
+@media all and (max-width: 670px) {
+  h2 , .menu h3 , .sita a{
+    font-size:30px;
+  }
+
+  h3 , .menu p , .food p{
+  font-size: 25px;
+  }
+
+  h4 , .ji{
+    font-size:20px;
+  }
+
+  h5{
+    font-size: 15px;
+  }
+
+  header span , header p{
+    font-size: 50px;
+  }
+
+  header{
+    height:80px;
+  }
+
+  .yes{
+    display: none;
+  }
+
+  .no{
+    display: block;
+  }
+
+  .lo2{
+    position: absolute;
+    top:-1px;
+  }
+
+  .home{
+    height: 160px;
+  }
+
+  .home h3{
+    position: absolute;
+    top:110px;
+    left:20px;
+  }
+
+  .concept{
+    height:370px;
+  }
+
+  .time{
+    height:240px;
+  }
+
+  .access{
+    height:370px;
+  }
+
+  .menu{
+    width:180px;
+    background-color: red;
+    opacity: 0.7;
+    padding: 10px;
+    line-height: 50px;
+    padding-top: 25px;
+  }
+
+  footer{
+    height:150px;
+}
+
+.osinagaki{
+  padding-bottom:120px;
+}
+
+.navi{
+  height:140px;
+}
+
+.tori , .nama , .vege , .niku , .orignal , .sina , .rice , .desert{
+  margin:25px 0;
+}
+
+.list{
+  margin:7px 0;
+}
+
+.list h4{
+  margin:2px 8px ;
+}
+
+.yakitori2{
+  position :absolute;
+  top: 520px;
+  right:70px;
+}
+
+.sasami2{
+  position :absolute;
+  top: 880px;
+  right:50px;
+}
+
+.nasu2{
+  position :absolute;
+  top: 1680px;
+  right:70px;
+}
+
+.be2{
+  position :absolute;
+  top: 2320px;
+  right:20px;
+}
+
+.yuzu2{
+  position :absolute;
+  top: 2910px;
+  right:35px;
+}
+
+.maru2{
+  position :absolute;
+  top: 3835px;
+  right:5px;
+}
+
+
+}
